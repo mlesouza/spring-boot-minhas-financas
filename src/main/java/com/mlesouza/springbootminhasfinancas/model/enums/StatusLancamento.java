dc3a7250a14ea225c619667243cfc22a8b182eb9
Marcelo Leonardo de Souza <mlesouza95@gmail.com>
2021-03-14 17:43:09 -0300

    adicionado entidade lancamento e biblioteca lombok ao projeto

---
+++ src/main/java/com/mlesouza/springbootminhasfinancas/model/enums/StatusLancamento.java
@@ -0,0 +1,7 @@
+package com.mlesouza.springbootminhasfinancas.model.enums;
+
+public enum StatusLancamento {
+    PENDENTE,
+    CANCELADO,
+    EFETIVADO
+}
---
+++ src/main/java/com/mlesouza/springbootminhasfinancas/model/enums/TipoLancamento.java
@@ -0,0 +1,8 @@
+package com.mlesouza.springbootminhasfinancas.model.enums;
+
+public enum TipoLancamento {
+
+    RECEITA,
+    DESPESA
+
+}
