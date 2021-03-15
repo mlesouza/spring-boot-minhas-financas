c626f77f6c80dc4f5fe84700717b313a946d1019
marcelo <mlesouza95@gmail.com>
2021-03-15 13:51:04 -0300

    refatorando os testes.

--- src/test/java/com/mlesouza/springbootminhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/mlesouza/springbootminhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -5,30 +5,66 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import java.util.Optional;
+
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioRepositoryTest {
 
     @Autowired
     UsuarioRepository repository;
 
+    @Autowired
+    TestEntityManager entityManager;
+
     @Test
     public void deveVerificarAExistenciaDeUmEmail() {
-        Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-        repository.save(usuario);
-        boolean result = repository.existsByEmail(usuario.getEmail());
-        Assertions.assertThat(result).isTrue();
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
+        boolean resultado = repository.existsByEmail(usuario.getEmail());
+        Assertions.assertThat(resultado).isTrue();
     }
 
     @Test
     public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
-        repository.deleteAll();
-        boolean result = repository.existsByEmail("usuario@email.com");
-        Assertions.assertThat(result).isFalse();
+        boolean resultado = repository.existsByEmail("usuario@email.com");
+        Assertions.assertThat(resultado).isFalse();
+    }
+
+    @Test
+    public void devePersistirUmUsuarioNaBaseDeDados() {
+        Usuario usuario = criarUsuario();
+        Usuario usuarioSalvo = repository.save(usuario);
+        Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+    }
+
+    @Test
+    public void deveBuscarUmUsuarioPorEmail() {
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
+        Optional<Usuario> resultado = repository.findByEmail("usuario@email.com");
+        Assertions.assertThat(resultado.isPresent()).isTrue();
+    }
+
+    @Test
+    public void deveRetornarVazioAoBuscarUsuarioPorEmailQuandoNaoExisteNaBase() {
+        Optional<Usuario> resultado = repository.findByEmail("usuario@email.com");
+        Assertions.assertThat(resultado.isPresent()).isFalse();
+    }
+
+    public static Usuario criarUsuario() {
+        return Usuario.builder()
+                .nome("usuario")
+                .email("usuario@email.com")
+                .senha("senha")
+                .build();
     }
 }
--- src/test/java/com/mlesouza/springbootminhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/mlesouza/springbootminhasfinancas/service/UsuarioServiceTest.java
@@ -3,9 +3,13 @@ package com.mlesouza.springbootminhasfinancas.service;
 import com.mlesouza.springbootminhasfinancas.exception.RegraNegocioException;
 import com.mlesouza.springbootminhasfinancas.model.entity.Usuario;
 import com.mlesouza.springbootminhasfinancas.model.repository.UsuarioRepository;
+import com.mlesouza.springbootminhasfinancas.service.impl.UsuarioServiceImpl;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,28 +17,31 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-    @Autowired
     UsuarioService service;
-
-    @Autowired
     UsuarioRepository repository;
+
+    @BeforeEach
+    public void setUp() {
+        repository = Mockito.mock(UsuarioRepository.class);
+        service = new UsuarioServiceImpl(repository);
+    }
 
     @Test
     public void deveValidarEmail() {
-        repository.deleteAll();
-        service.validarEmail("usuario@email.com");
+        Assertions.assertDoesNotThrow(() -> {
+            Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
+            service.validarEmail("usuario@email.com");
+        });
     }
 
     @Test
     public void deveRetornarErroQuandoExistirEmailCadastrado() {
-        Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-        repository.save(usuario);
-        Exception excecao = Assertions.assertThrows(Exception.class, () -> service.validarEmail("usuario@email.com"),
+       Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
+       Assertions.assertThrows(Exception.class, () -> service.validarEmail("usuario@email.com"),
                 "Já existe um usuário cadastrado com este email");
 
     }
